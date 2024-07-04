fba78260c40cd3ccd3b84187c1f62245f078ace7
NikitaShyshkov <super5nikitos@gmail.com>
2024-07-04 11:47:22 +0300

    Auth check main page

--- callories-frontend-app/src/components/CalorieTracker/CalorieTracker.jsx
+++ callories-frontend-app/src/components/CalorieTracker/CalorieTracker.jsx
@@ -5,15 +5,23 @@ import styles from './CalorieTracker.module.css';
 const CalorieTracker = () => {
     const progress = 1800; // сюда кол-во калорий
     const max = 2160; // сюда норму калорий за день
+    const user = JSON.parse(localStorage.getItem('user'));
 
     return (
         <div className={styles.calorieTracker}>
-            <CircularProgressBar value={progress} max={max} />
+            <CircularProgressBar value={user ? progress : 0} max={max} />
             <div className={styles.details}>
-                <div className={styles.calories}>{progress}</div>
+                <div className={styles.calories}>{user ? progress : 0}</div>
                 <div>ккал</div>
                 <div className={styles.icon}>🔥</div>
             </div>
+            {
+                user
+                ?
+                null
+                :
+                "Авторизуйтесь для отримання доступу до інформації"
+            }
         </div>
     );
 };
